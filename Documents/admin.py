b84ffbd701cfa7e214f9e0e7486cfafbfd831a9f
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-03-19 12:12:54 +0200

    added song type

--- Documents/admin.py
+++ Documents/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Copies
+from .models import Copies,SongType
 # Register your models here.
 
 admin.site.register(Copies)
+admin.site.register(SongType)
--- Documents/models.py
+++ Documents/models.py
@@ -2,6 +2,15 @@ from django.db import models
 import uuid
 from Accounts.models import Users
 # Create your models here.
+
+class SongType(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    name=models.CharField(max_length=255,null=False)
+    season=models.CharField(max_length=255,null=True,default='Ibihe Bisanzwe')
+
+    def __str__(self):
+        return self.name
+    
 class Copies(models.Model):
     id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     name=models.CharField(max_length=255,null=False)
@@ -9,7 +18,7 @@ class Copies(models.Model):
     part=models.CharField(max_length=255,default="Other")
     uploader=models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
     document=models.FileField(upload_to='songs_docs')
-
+    category=models.ForeignKey(SongType,on_delete=models.CASCADE,null=True,default='')
     def __str__(self):
         return self.name
-    
+
